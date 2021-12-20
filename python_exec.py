4fdda61870d05e8a59d13f33da80910397cbda36
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2021-12-20 11:21:11 +0100

    Python exec built-in
    Sample real usages of the Python exec built-in

---
+++ python_exec.py
@@ -0,0 +1,11 @@
+import os
+def cc(n):
+    print(n)
+#field = [f for f in reacted._meta.fields]
+#field = [exec(f"{cc(f)}") for f in reacted._meta.fields]
+#print(field,len(field))
+react = "getcwd()"
+exec(f"name=os.{react}\ncc(name)\nprint('EXEC',name)")
+exec("print('helo')\n"
+    "print('world')"
+)
